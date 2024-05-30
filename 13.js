eb2a9ac321d7a184141cfb3289d92ba393f31042
Harshvardhan <shreeharshpb11@gmail.com>
2024-05-30 11:08:35 +0530

    Find the factorial of a number.

---
+++ 13.js
@@ -0,0 +1,9 @@
+// Find the factorial of a number.
+
+const number = 7
+let factorial = 1
+for (i = 1; i <= number; i++) {
+    factorial = factorial * i
+    console.log(factorial)
+}
+console.log(factorial)
