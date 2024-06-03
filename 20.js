d5995695251d9113359f31f7971ba00d6a82d4b6
Harshvardhan <shreeharshpb11@gmail.com>
2024-06-03 12:20:24 +0530

    revese the each word from sentences using for loop

--- 20.js
+++ 20.js
@@ -2,14 +2,34 @@
 // hello everyone 
 // olleh enoyteve
 
-const str = 'hello everyone'
-const splitStr = str.split(' ')
+const str1 = 'harshvardhan bhosale'
+const splitStr = str1.split(' ')
 console.log(splitStr)
 const newStr = splitStr.map((word) => {
     return word.split('').reverse().join('')
 
 })
 
-console.log(newStr)
+console.log(newStr.join(' '))
 
 
+
+
+// using for loop
+
+let newString = 'hey coder bring party here'
+let newRes = ''
+let finalAns = ''
+
+for (let i = 0; i < newString.length; i++) {
+    if (newString[i] == ' ') {
+        finalAns = finalAns + ' ' + newRes + ''
+        newRes = ''
+    } else {
+        newRes = newString[i] + newRes
+    }
+}
+
+finalAns = finalAns + ' ' + newRes
+
+console.log(finalAns.trim())
